dba524d1d3037f1914d06525010e43d6586da6de
vchenpeng <vitoc@qq.com>
2016-07-19 10:21:48 +0800

    服务端

---
+++ AmazingChat/AmazingChat.Server/ChatHub.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace AmazingChat.Server
+{
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        static List<Message> _allMsg = new List<Message>();
+
+        public void SendMessage(Message msg)
+        {
+            msg.CreateTime = DateTime.Now;
+            _allMsg.Add(msg);
+            Clients.All.Talk(msg);
+            Clients.All.Notice(msg);
+            Notice("这是一个通知");
+        }
+
+        public void Notice(string info)
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+            context.Clients.All.Notice(info);
+        }
+
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+    }
+}
